a1e99fe3c4b54647c3bd2922398b06507b8ba8e1
Rafał Rzepecki <divided.mind@gmail.com>
2014-01-24 03:51:27 +0100

    Yay! Mode is switched to XML

--- generate.rb
+++ generate.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'nokogiri'
+#require 'nokogiri/pp'
+require 'pp'
 
 jspath = '/usr/share/apps/katepart/syntax/javascript.xml'
 xmlpath = '/usr/share/apps/katepart/syntax/xml.xml'
@@ -8,15 +10,25 @@ xmlpath = '/usr/share/apps/katepart/syntax/xml.xml'
 js = Nokogiri::XML(File.read jspath)
 xml = Nokogiri::XML(File.read xmlpath)
 
-js.traverse do |elem|
+xml.root.traverse do |elem|
   %w(name attribute context fallthroughContext).each do |a|
     if elem.has_attribute? a
-      elem[a] += " (JS)" unless elem[a].start_with? '#'
+      elem[a] += " (XML)" unless elem[a].start_with? '#'
     end
   end
 end
 
-jsx = Nokogiri::XML(File.read 'container.xml')
-jsx.root.add_child js.at('highlighting')
+jsx = xml.dup # copy the document to get the entities
+jsx.root.remove
+jsx.add_child Nokogiri::XML(File.read 'container.xml').root
+
+patches = jsx.at('highlighting').swap js.at('highlighting')
+jsx.at('contexts') << xml.at('contexts').children
+jsx.at('itemDatas') << xml.at('itemDatas').children
+
+patches.search('context').each do |context|
+  p "context[name=\"#{context['name']}\"]"
+  jsx.at("context[name=\"#{context['name']}\"]").child.before context.children
+end
 
 File.write 'jsx.xml', jsx.to_xml
