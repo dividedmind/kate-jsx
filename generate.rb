73905f1cf594aff264481b15ad93e4bafd3e7db7
Rafał Rzepecki <divided.mind@gmail.com>
2014-01-24 02:50:32 +0100

    JS elements renamed

--- generate.rb
+++ generate.rb
@@ -8,6 +8,14 @@ xmlpath = '/usr/share/apps/katepart/syntax/xml.xml'
 js = Nokogiri::XML(File.read jspath)
 xml = Nokogiri::XML(File.read xmlpath)
 
+js.traverse do |elem|
+  %w(name attribute context fallthroughContext).each do |a|
+    if elem.has_attribute? a
+      elem[a] += " (JS)" unless elem[a].start_with? '#'
+    end
+  end
+end
+
 jsx = Nokogiri::XML(File.read 'container.xml')
 jsx.root.add_child js.at('highlighting')
 
